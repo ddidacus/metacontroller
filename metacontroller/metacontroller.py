6d32c864cb4ae0ddb7262c7fa87822595a8cf99c
lucidrains <lucidrains@gmail.com>
2025-12-30 06:34:11 -0800

    start with the decoder that generates the hyperweights that modulates the residual stream

--- metacontroller/metacontroller.py
+++ metacontroller/metacontroller.py
@@ -1,11 +1,19 @@
 import torch
 from torch import nn
-from torch.nn import Module, GRU
+from torch.nn import Module, GRU, Identity
 import torch.nn.functional as F
+
+# einops
+
+import einx
+from einops import einsum
+from einops.layers.torch import Rearrange
 
 # external modules
 
 from x_transformers import Decoder
+from x_mlps_pytorch import Feedforwards
+
 from discrete_continuous_embed_readout import Embed, Readout
 
 # helper functions
@@ -13,10 +21,51 @@ from discrete_continuous_embed_readout import Embed, Readout
 def exists(v):
     return v is not None
 
+def identity(t):
+    return t
+
 def default(v, d):
     return v if exists(v) else d
 
-# modules
+# meta controller
+
+class MetaController(Module):
+    def __init__(
+        self,
+        dim_latent,
+        *,
+        decoder_expansion_factor = 2.,
+        decoder_depth = 1,
+        hypernetwork_low_rank = 16
+    ):
+        super().__init__()
+
+        dim_decoder_hidden = int(dim_latent * decoder_expansion_factor)
+
+        assert hypernetwork_low_rank < dim_latent
+
+        self.decoder = Feedforwards(
+            dim_in = dim_latent,
+            dim = dim_decoder_hidden,
+            depth = decoder_depth,
+            dim_out = 2 * hypernetwork_low_rank * dim_latent
+        )
+
+        self.to_hyper_network_weights = Rearrange('... (two d r) -> two ... d r', two = 2, r = hypernetwork_low_rank)
+
+    def forward(
+        self,
+        latent
+    ):
+
+        decoder_out = self.decoder(latent)
+
+        w1, w2 = self.to_hyper_network_weights(decoder_out)
+        hypernetwork_weight = einsum(w1, w2, '... i r, ... j r -> ... i j')
+
+        control_signal = einsum(latent, hypernetwork_weight, '... d1, ... d1 d2 -> ... d1')
+
+        return latent + control_signal
 
 # main transformer, which is subsumed into the environment after behavioral cloning
 
@@ -52,6 +101,7 @@ class Transformer(Module):
     def forward(
         self,
         ids,
+        meta_controller: Module = Identity(),
         return_latents = False
     ):
         embed = self.embed(ids)
@@ -59,6 +109,8 @@ class Transformer(Module):
         latents = self.lower_body(embed)
 
         # meta controller acts on latents here
+
+        modified_latents = meta_controller(latents)
 
         # modified latents sent back
 
--- tests/test_metacontroller.py
+++ tests/test_metacontroller.py
@@ -1,18 +1,21 @@
 
 import torch
-from metacontroller.metacontroller import Transformer
+from metacontroller.metacontroller import Transformer, MetaController
 
 def test_metacontroller():
 
     ids = torch.randint(0, 256, (1, 1024))
 
     model = Transformer(
-        256,
+        512,
         embed = dict(num_discrete = 256),
         lower_body = dict(depth = 2,),
         upper_body = dict(depth = 2,),
         readout = dict(num_discrete = 256)
     )
 
-    logits = model(ids)
+    meta_controller = MetaController(512)
+
+    logits = model(ids, meta_controller = meta_controller)
+
     assert logits.shape == (1, 1024, 256)
